f2e4b6c113ae86d75410af6478876778f1ed0aaf
Colin Rupp <colin@thinkful.com>
2018-08-27 18:15:39 -0700

    initialized!

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const state = {
+	winState: false,
+	tokenState: true,
+	cellId: []
 };
 
+
 // State modification functions
+function placeToken() {
+	if (tokenStateX === false) {
+		${target}.html('O'); 
+		state.tokenStateX = true;
+	} else {
+		$(target).html('X');
+		tokenStateX = false;
+	}
+}
+
+
+
+function resetGameBoard() {
+
+}
 
 // Render functions
+function render() {
+
+}	
 
 // Event Listeners
+
+$('.game').on('click', '.cell', function(event) {
+		const cell = $(this).attr('id');
+		
+	});
+
